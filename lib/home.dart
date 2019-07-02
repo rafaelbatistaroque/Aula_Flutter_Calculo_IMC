349c59572f789ea2c7dea77d26474ad1e73f39f8
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-02 11:28:25 -0400

    Refactoring

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController pesoCrtl = new TextEditingController();
   TextEditingController alturaCrtl = new TextEditingController();
   String _informe = "Digite seu peso e sua altura";
@@ -19,21 +18,28 @@ class _HomeState extends State<Home> {
       _informe = "Digite seu peso e sua altura";
     });
   }
+
 //Calculo IMC
   void _calcularIMC() {
     setState(() {
       double weight = double.parse(pesoCrtl.text);
       double height = double.parse(alturaCrtl.text) / 100;
       double imc = weight / (height * height);
-      if (imc < 18.6) _informe = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
-      else if (imc >= 18.6 && imc < 24.9) _informe = "Peso ideal (${imc.toStringAsPrecision(3)})";
-      else if (imc >= 24.9 && imc < 29.9) _informe = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
-      else if (imc >= 29.9 && imc < 34.9) _informe = "Obesidade grau I (${imc.toStringAsPrecision(3)})";
-      else if (imc >= 34.9 && imc < 39.9) _informe = "Obesidade grau II (${imc.toStringAsPrecision(3)})";
-      else if (imc >= 40) _informe = "Obesidade grau III (${imc.toStringAsPrecision(3)})";
-
+      if (imc < 18.6)
+        _informe = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 18.6 && imc < 24.9)
+        _informe = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 24.9 && imc < 29.9)
+        _informe = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 29.9 && imc < 34.9)
+        _informe = "Obesidade grau I (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 34.9 && imc < 39.9)
+        _informe = "Obesidade grau II (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 40)
+        _informe = "Obesidade grau III (${imc.toStringAsPrecision(3)})";
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
